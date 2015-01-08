44b7082e048a004f43cb10744bbf903f7bd7872a
Bryan Cornies <bryan@sixsquarestudio.com>
2015-01-07 22:17:37 -0700

    Support deploying to local concrete5 server

--- modules/docks/concrete5/manifests/config.pp
+++ modules/docks/concrete5/manifests/config.pp
@@ -1,5 +1,10 @@
 class concrete5::config {
 
+  file { "${bertha::website_home}/deploy.xml":
+    ensure => file,
+    content => template('concrete5/server/deploy.xml.erb'),
+  }
+
   file { "${bertha::website_home}/config/database.php":
     ensure  => file,
     content => template('concrete5/database.php.erb'),
