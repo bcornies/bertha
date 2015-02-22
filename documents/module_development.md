b1101b0c01509d1f13d96cf428149025592c83d7
Bryan Cornies <bryan@sixsquarestudio.com>
2015-02-21 21:37:27 -0700

    Move logic for iterating on each lib and adding it to include into Core

--- documents/module_development.md
+++ documents/module_development.md
@@ -6,21 +6,21 @@ A custom CMS module is nothing more than a Puppet module that...
 - manages the development server configuration
 - defines deployment logic
 
-A typical CMS module must have the following minimum structure:
+A CMS module must have the following minimum structure:
 
     `files`
       |- `server`
           |- `Puppetfile` (required!)
     `manifests`
-      |- `init.pp` (required!) <- declares any necessary parameters and includes class declared in other manifsts
+      |- `init.pp` (required!) <- declares any necessary parameters and includes class declared in other manifests
       |- `theme.pp` <- manages all files and directories required to bootstrap a skeleton theme for the CMS
       |- `config.pp` <- manages any configuration file templates for connecting to a CMS database (for example)
       |- `tools.pp` <- ensures any client tools that are required to work with CMS are installed locally
     `templates`
       |- `server`
-          |- `deploy.xml.erb`
-          |- `site.pp.erb`
-      |- `css.php.erb`
+          |- `deploy.xml.erb` <- A deployment engine with logic for how to deploy and configure for all environments
+          |- `site.pp.erb` (required!) <- Puppet manifest for provisioning development virtual machine to run CMS
+      |- `css.php.erb` <-
       |- `js.php.erb`
 
 When creating a new CMS module for use in Puppet, you need to address four requirements:
--- modules/core/bertha/manifests/skeleton.pp
+++ modules/core/bertha/manifests/skeleton.pp
@@ -26,14 +26,45 @@ class bertha::skeleton {
 		source  => 'puppet:///modules/bertha/main.js',
 	}
 
-	file { "${bertha::website_home}/includes/css.php":
+	file { [
+		"${bertha::website_home}/includes/css.php",
+		"${bertha::website_home}/includes/js.php",
+	]:
 		ensure  => file,
-		content => template("${::cms}/css.php.erb"),
 	}
 
-	file { "${bertha::website_home}/includes/js.php":
-		ensure  => file,
-		content => template("${::cms}/js.php.erb"),
+	$theme_path = chomp(template("${::cms}/theme_path.erb"))
+
+	$::libraries['css'].each |$css_library| {
+
+		file_line { "css_include_$css_library":
+			ensure => present,
+			line   => "<link rel=\"stylesheet\" type=\"text/css\" href=\"${theme_path}/css/lib/${css_library}.css\">",
+			path   => "${bertha::website_home}/includes/css.php",
+		}
+
+	}
+
+	file_line { "css_include_main":
+		ensure => present,
+		line   => "<link rel=\"stylesheet\" type=\"text/css\" href=\"${theme_path}/css/main.css\">",
+		path   => "${bertha::website_home}/includes/css.php",
+	}
+
+	$::libraries['js'].each |$js_library| {
+
+		file_line { "js_include_$js_library":
+			ensure => present,
+			line   => "<script type=\"text/javascript\" src=\"${theme_path}/js/lib/${js_library}.js\"></script>",
+			path   => "${bertha::website_home}/includes/js.php",
+		}
+
+	}
+
+	file_line { "js_include_main":
+		ensure => present,
+		line   => "<script type=\"text/javascript\" src=\"${theme_path}/js/main.js\"></script>",
+		path   => "${bertha::website_home}/includes/js.php",
 	}
 
 }
